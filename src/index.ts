b652ac5f06c5766b52e31fb5bd0a5109dff9b629
Osama Hassanein <player1os@gmail.com>
2017-06-05 06:32:40 +0200

    Temporary fix to unify declaration files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,55 @@
+/* tslint:disable max-classes-per-file */
+
 // Load scoped modules.
-import EntityExistsError from '#/src/error/entity_exists'
-import EntityNotFoundError from '#/src/error/entity_not_found'
+import BaseError from '@player1os/base-error'
+
+// Expose error class.
+export class EntityExistsError extends BaseError {
+	fieldName
+	constraint
+	fields
+	values
+	detail
+
+	constructor(knexError) {
+		// Call parent constructor.
+		super('An entity already exists with the submitted unique field values')
+
+		// Parse knex error.
+		const matches = knexError.detail.match(/^Key \((.*)\)=\((.*)\) already exists.$/)
+		const fields = matches[1].split(', ')
+
+		// Fill error properties.
+		this.constraint = knexError.constraint
+		this.fields = fields.map((field) => {
+			return `"${knexError.table}.${field}"`
+		}).join(', ')
+		this.values = matches[2].split(', ').map((value) => {
+			return `'${value}'`
+		}).join(', ')
+
+		this.detail = fields.reduce((accumulator, field) => {
+			accumulator[field] = {
+				input: this.values,
+				type: 'any.db_unique_constraint',
+				message: `A "${knexError.table}" entity already exists with the same value ${this.values}`
+					+ ` in the ${this.fields} field${(this.fields.length > 1) ? 's' : ''}`,
+			}
+			return accumulator
+		}, {})
+	}
+}
+
+// Expose error class.
+export class EntityNotFoundError extends BaseError {
+	constructor() {
+		// Call parent constructor.
+		super('No entity exists that matches the submitted query')
+	}
+}
 
 // Load npm modules.
 import * as lodash from 'lodash'
-
-// Expose the error classes.
-export {
-	EntityExistsError,
-	EntityNotFoundError,
-}
 
 // Expose the base model object.
 export default {
