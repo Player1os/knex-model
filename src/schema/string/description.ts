89c58b11d262a6af41d91087f8e1173d0ef2b98c
Osama Hassanein <player1os@gmail.com>
2017-06-29 08:00:08 +0200

    FIx type definition

--- src/schema/string/description.ts
+++ src/schema/string/description.ts
@@ -1,5 +1,8 @@
 // Load app modules.
 import anyStringSchema from '.../src/schema/string/any'
 
+// Load npm modules.
+import * as Joi from 'joi'
+
 // Expose the validation schema.
-export default anyStringSchema.max(512)
+export default anyStringSchema.max(512) as Joi.StringSchema
