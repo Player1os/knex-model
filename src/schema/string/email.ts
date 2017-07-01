f639350a7727c643e80c8a8e89343eddea90af82
Osama Hassanein <player1os@gmail.com>
2017-07-01 15:59:36 +0200

    Add reg exp

--- src/schema/string/email.ts
+++ src/schema/string/email.ts
@@ -1,6 +1,18 @@
 // Load npm modules.
 import * as Joi from 'joi'
 
-// Expose the validation schema.
 // TODO: Verify if this matches the legacy reg exp.
+// const regExp = new RegExp(
+// 	'^'
+// 	+ "[-a-z0-9~!$%^&*_=+}{\\'?]+(\\.[-a-z0-9~!$%^&*_=+}{\\'?]+)*"
+// 	+ '@'
+// 	+ '([a-z0-9_][-a-z0-9_]*(\\.[-a-z0-9_]+)*'
+// 	+ '\\.'
+// 	+ '(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])'
+// 	+ '|([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}))(:[0-9]{1,5})?'
+// 	+ '$',
+// 	'i',
+// )
+
+// Expose the validation schema.
 export default Joi.string().email().max(256)
