f96b73b12f6bc0e11633e5699627f21e133aa206
Osama Hassanein <player1os@gmail.com>
2017-07-01 14:09:30 +0200

    Update the library's exports

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import validate from '.../src/validate'
 // Expose the schemas.
 export { default as booleanSchema } from '.../src/schema/boolean'
 export { default as dateSchema } from '.../src/schema/date'
-export { default as filterExpressionItemSchema } from '.../src/schema/filter_expression_item'
+export { default as filterExpressionSchema } from '.../src/schema/filter_expression'
 export { default as bigIntegerKeySchema } from '.../src/schema/key/big_integer'
 export { default as integerKeySchema } from '.../src/schema/key/integer'
 export { default as nonNegativeBigIntegerNumberSchema } from '.../src/schema/number/non_negative_big_integer'
 export { default as nonNegativeIntegerNumberSchema } from '.../src/schema/number/non_negative_integer'
 export { default as labelStringValueObjectSchema } from '.../src/schema/object/label_string_value'
+export { default as requiredKeysObjectSchema } from '.../src/schema/object/required_keys'
+export { default as parsedValidationErrorSchema } from '.../src/schema/parsed_validation_error'
 export { default as anyStringSchema } from '.../src/schema/string/any'
 export { default as descriptionStringSchema } from '.../src/schema/string/description'
 export { default as emailStringSchema } from '.../src/schema/string/email'
@@ -20,11 +22,8 @@ export { default as labelStringSchema } from '.../src/schema/string/label'
 export { default as tokenStringSchema } from '.../src/schema/string/token'
 export { default as webTokenStringSchema } from '.../src/schema/string/web_token'
 
-// Expose the error class.
-export {
-	IDetailItem as IValidationErrorDetailItem,
-	default as ValidationError,
-} from '.../src/error'
+// Expose the parse validation error function.
+export { default as parseValidationError } from '.../src/parse_validation_error'
 
 // Expose the validate function.
 export default validate
