e6aec61615a92d4fed670ac23b2eb208c8d27a65
Osama Hassanein <osama.hassanein@cellense.com>
2017-09-14 16:03:55 +0200

    Allow custom validation errors

--- src/error.ts
+++ src/error.ts
@@ -29,35 +29,59 @@ export default class ValidationError extends BaseError {
 	 * Parses the joi validation error and use to create an object of error item arrays.
 	 * @param joiValidationError A validation error thrown by a joi assertion.
 	 */
-	constructor(joiValidationError: Joi.ValidationError) {
+	constructor(errorObject: Joi.ValidationError)
+	constructor(errorObject: object) {
+		// Store a typecasted version of the error object.
+		const joiValidationError = errorObject as Joi.ValidationError
+
 		// Copy the message from the joi validation error.
-		super(joiValidationError.message)
+		super(joiValidationError.message || 'A validation error had occured')
 
-		// Store the original value, that was validated against.
-		const value = joiValidationError._object
+		if (joiValidationError.isJoi) {
+			// Store the original value, that was validated against.
+			const value = joiValidationError._object
 
-		// Fill the details object.
-		this.details = joiValidationError.details.reduce((map, joiValidationErrorItem) => {
-			// Store the path within the original value.
-			const path = joiValidationErrorItem.path
+			// Fill the details object.
+			this.details = joiValidationError.details.reduce((map, joiValidationErrorItem) => {
+				// Store the path within the original value.
+				const path = joiValidationErrorItem.path
 
-			// Parse the error item.
-			const errorItem: IErrorItem = {
-				value: (value instanceof Object) && (path in value)
-					? lodash.get(value, path)
-					: value,
-				type: joiValidationErrorItem.type,
-				message: joiValidationErrorItem.message,
+				// Parse the error item.
+				const errorItem: IErrorItem = {
+					value: (value instanceof Object) && (path in value)
+						? lodash.get(value, path)
+						: value,
+					type: joiValidationErrorItem.type,
+					message: joiValidationErrorItem.message,
+				}
+
+				// Add to the array of error items for the given path.
+				if (!(path in map)) {
+					map[path] = []
+				}
+				map[path].push(errorItem)
+
+				// Return the accumulated object.
+				return map
+			}, {})
+		} else {
+			this.details = lodash.mapValues(errorObject, (errorObjectItem, errorObjectKey) => {
+				return lodash.isArray(errorObjectItem)
+					? errorObjectItem.map((errorObjectItemEntry) => {
+						return {
+							value: `[VALUE OF: ${errorObjectKey.toString()}]`,
+							type: 'custom_validation_error',
+							message: errorObjectItemEntry.toString() as string,
+						} as IErrorItem
+					})
+					: {
+						value: `[VALUE OF: ${errorObjectKey.toString()}]`,
+						type: 'custom_validation_error',
+						message: errorObjectItem.toString() as string,
+					} as IErrorItem
+			}) as {
+				readonly [key: string]: IErrorItem[],
 			}
-
-			// Add to the array of error items for the given path.
-			if (!(path in map)) {
-				map[path] = []
-			}
-			map[path].push(errorItem)
-
-			// Return the accumulated object.
-			return map
-		}, {})
+		}
 	}
 }
